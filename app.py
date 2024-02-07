39032bfccd2b24f6bbda1ff44669d6c974db0071
Maria Zyatyugina <81264975+mashazya@users.noreply.github.com>
2024-02-07 09:06:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -285,8 +285,9 @@ def login_to_website(username, password):
   
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     #chrome_options.add_argument("--window-size=1920,1080")
     
     # Initialize ChromeDriverManager with the desired version
