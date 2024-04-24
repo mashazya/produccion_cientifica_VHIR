b4594097f4ebbabf5d614f348f2ae10d61948427
Maria Zyatyugina <81264975+mashazya@users.noreply.github.com>
2024-04-24 15:56:31 +0200

    change q and if

--- app.py
+++ app.py
@@ -394,8 +394,15 @@ def run_scrapping(if_xlm):
             
             colif = 'IF{}'.format(str(if_year))
             colq = 'Q{}'.format(str(if_year))
-            if_xlm.at[idx, colif] = str(impact_factor)
-            if_xlm.at[idx, colq] = str(quantile)
+            if not impact_factor is None:
+              if_xlm.at[idx, colif] = str(impact_factor)
+            else:
+              if_xlm.at[idx, colif] = impact_factor
+
+            if not quantile is None:
+              if_xlm.at[idx, colq] = str(quantile)
+            else:
+              if_xlm.at[idx, colq] = quantile
         percent_complete = int((idx+1)*100/len(if_xlm))
     my_bar.progress(100, text=progress_text)
     my_bar.empty()
