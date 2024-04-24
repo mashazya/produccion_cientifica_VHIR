e68d3047021fc84b9552adec2530ad1538c54177
Maria Zyatyugina <81264975+mashazya@users.noreply.github.com>
2024-04-24 16:45:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -383,9 +383,9 @@ def run_scrapping(if_xlm):
     authenticated_driver = login_to_website(username, password)
     if f'IF{if_year}' not in if_xlm.columns:
         if f'IF{if_year}' not in if_xlm.columns:
-          if_xlm[f'IF{if_year}'] = pd.NA for pandas >=1.0.0
+          if_xlm[f'IF{if_year}'] = pd.NA
         if f'Q{if_year}' not in if_xlm.columns:
-            if_xlm[f'Q{if_year}'] = pd.NA for pandas >=1.0.0
+            if_xlm[f'Q{if_year}'] = pd.NA
         if_xlm[f'IF{if_year}'] = if_xlm[f'IF{if_year}'].astype('object')
         if_xlm[f'Q{if_year}'] = if_xlm[f'Q{if_year}'].astype('object')
     progress_text = "Buscando artículos...."
